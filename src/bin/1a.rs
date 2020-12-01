a1a3c90374f46fa80bf155b48b5b02a262a0a9a1
Henrik Alsér <henrik.alser@me.com>
2020-12-01 21:59:12 +0100

    Skip

--- src/bin/1a.rs
+++ src/bin/1a.rs
@@ -2,9 +2,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
 
 const FILE: &str = "./input/1.txt";
-const SUM: i64 = 2020;
+const SUM: i32 = 2020;
 
-fn read_lines<R: Read>(io: R) -> Result<Vec<i64>, Error> {
+fn read_lines<R: Read>(io: R) -> Result<Vec<i32>, Error> {
     BufReader::new(io)
         .lines()
         .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
@@ -18,9 +18,9 @@ fn main() -> Result<(), Error> {
         .enumerate()
         .find_map(|(i, &x)| {
             vec.iter()
-                .enumerate()
-                .find(|(ii, &y)| i != *ii && x + y == SUM)
-                .map(|(_, &y)| (x, y))
+                .skip(i + 1)
+                .find(|&y| x + y == SUM)
+                .map(|&y| (x, y))
         })
         .map(|(x, y)| x * y);
 
@@ -28,5 +28,6 @@ fn main() -> Result<(), Error> {
         Some(n) => println!("{:?}", n),
         None => println!("No combination found!"),
     }
+
     Ok(())
 }
--- src/bin/1b.rs
+++ src/bin/1b.rs
@@ -2,9 +2,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
 
 const FILE: &str = "./input/1.txt";
-const SUM: i64 = 2020;
+const SUM: i32 = 3030;
 
-fn read_lines<R: Read>(io: R) -> Result<Vec<i64>, Error> {
+fn read_lines<R: Read>(io: R) -> Result<Vec<i32>, Error> {
     BufReader::new(io)
         .lines()
         .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
@@ -17,11 +17,11 @@ fn main() -> Result<(), Error> {
         .iter()
         .enumerate()
         .find_map(|(i, &x)| {
-            vec.iter().enumerate().find_map(|(ii, &y)| {
+            vec.iter().enumerate().skip(i + 1).find_map(|(ii, &y)| {
                 vec.iter()
-                    .enumerate()
-                    .find(|(iii, &z)| i != ii && i != *iii && ii != *iii && x + y + z == SUM)
-                    .map(|(_, &z)| (x, y, z))
+                    .skip(ii + 1)
+                    .find(|&z| x + y + z == SUM)
+                    .map(|&z| (x, y, z))
             })
         })
         .map(|(x, y, z)| x * y * z);
@@ -30,5 +30,6 @@ fn main() -> Result<(), Error> {
         Some(n) => println!("{:?}", n),
         None => println!("No combination found!"),
     }
+
     Ok(())
 }
