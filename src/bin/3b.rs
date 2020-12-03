910f0f3aa768b6734c43cf1f81efec4eb0564669
Henrik Alsér <henrik.alser@me.com>
2020-12-03 22:34:44 +0100

    Cleanup

--- src/bin/3b.rs
+++ src/bin/3b.rs
@@ -1,6 +1,17 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
 
+fn count_trees(rows: &Vec<String>, slope: &(usize, usize)) -> usize {
+    let (xs, ys) = slope;
+    rows.iter()
+        .enumerate()
+        .filter(|(y, r)| {
+            let x = ((y / ys) * xs) % r.chars().count();
+            (y % ys == 0) && (r.chars().nth(x) == Some('#'))
+        })
+        .count()
+}
+
 fn main() -> Result<(), Error> {
     let rows = BufReader::new(File::open("./input/3.txt")?)
         .lines()
@@ -8,16 +19,7 @@ fn main() -> Result<(), Error> {
     let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
     let trees = slopes
         .iter()
-        .map(|(xs, ys)| {
-            rows.iter()
-                .enumerate()
-                .filter(|(y, r)| {
-                    (y % ys == 0)
-                        && (r.chars().nth(((y / ys) * xs) % r.chars().count()) == Some('#'))
-                })
-                .count()
-        })
-        .fold(1, |acc, s| acc * s);
+        .fold(1, |acc, slope| acc * count_trees(&rows, slope));
     println!("{:?}", trees);
     Ok(())
 }
