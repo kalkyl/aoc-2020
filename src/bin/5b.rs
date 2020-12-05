23633bff9cfbea007690380d9ef1fcc8641c0f73
Henrik Alsér <henrik.alser@me.com>
2020-12-05 11:54:58 +0100

    Cleanup

--- src/bin/5b.rs
+++ src/bin/5b.rs
@@ -1,19 +1,19 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
 
-fn mid((a, b): &(usize, usize)) -> usize {
+fn mid(a: usize, b: usize) -> usize {
     (b - a) / 2 + 1
 }
 
 fn seat_id_from_str(s: &str) -> usize {
-    let row = s.chars().take(7).fold((0, 127), |acc, c| match c {
-        'F' => (acc.0, acc.1 - mid(&acc)),
-        'B' => (acc.0 + mid(&acc), acc.1),
+    let row = s.chars().take(7).fold((0, 127), |(a, b), c| match c {
+        'F' => (a, b - mid(a, b)),
+        'B' => (a + mid(a, b), b),
         _ => panic!("Invalid input"),
     });
-    let col = s.chars().skip(7).fold((0, 7), |acc, c| match c {
-        'L' => (acc.0, acc.1 - mid(&acc)),
-        'R' => (acc.0 + mid(&acc), acc.1),
+    let col = s.chars().skip(7).fold((0, 7), |(a, b), c| match c {
+        'L' => (a, b - mid(a, b)),
+        'R' => (a + mid(a, b), b),
         _ => panic!("Invalid input"),
     });
     row.0 * 8 + col.0
