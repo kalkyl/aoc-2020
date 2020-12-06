b8bd98387d6c1f032412bc06d983e18be4d7f00b
Henrik Alsér <henrik.alser@me.com>
2020-12-06 09:20:26 +0100

    Cleanup

--- src/bin/6b.rs
+++ src/bin/6b.rs
@@ -4,17 +4,16 @@ fn unique(s: &str) -> HashSet<char> {
     s.chars().filter(|&c| c != '\n').collect()
 }
 
-fn common(s: &str) -> HashSet<char> {
+fn count_common(s: &str) -> usize {
     unique(s)
         .iter()
         .filter(|&&c| s.lines().all(|l| l.contains(c)))
-        .cloned()
-        .collect()
+        .count()
 }
 
 fn main() -> Result<(), Error> {
     let input = read_to_string("./input/6.txt")?;
-    let result = input.split("\n\n").fold(0, |acc, s| acc + common(s).len());
+    let result = input.split("\n\n").fold(0, |acc, s| acc + count_common(s));
     println!("{}", result);
     Ok(())
 }
