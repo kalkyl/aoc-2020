352aeacb86ebb26436b8dc02bb2eb52fdb1165f9
Henrik Alsér <henrik.alser@me.com>
2020-12-06 14:23:07 +0100

    Cleanup

--- src/bin/5b.rs
+++ src/bin/5b.rs
@@ -1,14 +1,11 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
 
-fn step((a, b): (usize, usize), c: char) -> (usize, usize) {
-    match c {
+fn seat_id_from_str(s: &str) -> usize {
+    let step = |(a, b), c| match c {
         'F' | 'L' => (a, b - (b - a + 1) / 2),
         _ => (a + (b - a + 1) / 2, b),
-    }
-}
-
-fn seat_id_from_str(s: &str) -> usize {
+    };
     let row = s.chars().take(7).fold((0, 127), step).0;
     let col = s.chars().skip(7).fold((0, 7), step).0;
     row * 8 + col
