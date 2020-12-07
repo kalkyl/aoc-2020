a309b88ca5a5fba80cf5b9a002f365cb4d188490
Henrik Alsér <henrik.alser@me.com>
2020-12-07 22:46:38 +0100

    Cleanup

--- src/bin/1a.rs
+++ src/bin/1a.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind::InvalidData};
 
 fn main() -> Result<(), Error> {
-    let expenses: Vec<i32> = BufReader::new(File::open("./input/1.txt")?)
+    let expenses = BufReader::new(File::open("./input/1.txt")?)
         .lines()
         .map(|l| l.and_then(|v| v.parse().map_err(|e| Error::new(InvalidData, e))))
         .collect::<Result<Vec<i32>, _>>()?;
