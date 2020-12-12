431cd05d36e17683a447738051d05997b1183f52
Henrik Alsér <henrik.alser@me.com>
2020-12-12 21:52:29 +0100

    Cleanup

--- src/bin/8a.rs
+++ src/bin/8a.rs
@@ -1,46 +1,37 @@
 use std::io::{BufRead, BufReader, Error};
 use std::{collections::HashSet, fs::File};
 
-fn instruction_from_str(s: &str) -> Result<(&str, i32), ()> {
+fn instruction_from_string(s: String) -> Result<(String, i32), ()> {
     match s.split(' ').collect::<Vec<_>>()[..] {
-        [cmd, arg] => Ok((cmd, arg.parse().map_err(|_| ())?)),
+        [cmd, arg] => Ok((cmd.to_owned(), arg.parse::<i32>().unwrap())),
         _ => Err(()),
     }
 }
 
-fn run(instructions: &[String]) -> Option<i32> {
-    (0..)
-        .scan(
-            (HashSet::new(), 0, 0),
-            |(executed, line, acc), _| match executed.insert(*line) {
-                true => {
-                    match instructions
-                        .get(*line as usize)
-                        .and_then(|s| instruction_from_str(s).ok())
-                    {
-                        Some((cmd, arg)) => match cmd {
-                            "acc" => {
-                                *acc += arg;
-                                *line += 1;
-                            }
-                            "jmp" => *line += arg,
-                            _ => *line += 1,
-                        },
-                        _ => return None,
-                    }
-                    Some(*acc)
+fn run(instructions: &[(String, i32)]) -> i32 {
+    let (mut line, mut acc) = (0, 0);
+    let mut executed = HashSet::new();
+    while executed.insert(line) {
+        if let Some((cmd, arg)) = instructions.get(line as usize) {
+            match cmd.as_str() {
+                "acc" => {
+                    acc += arg;
+                    line += 1;
                 }
-                _ => None,
-            },
-        )
-        .last()
+                "jmp" => line += arg,
+                _ => line += 1,
+            }
+        }
+    }
+    acc
 }
 
 fn main() -> Result<(), Error> {
     let instructions = BufReader::new(File::open("./input/8.txt")?)
         .lines()
+        .map(|l| l.map(|s| instruction_from_string(s).unwrap()))
         .collect::<Result<Vec<_>, _>>()?;
     let acc = run(&instructions);
-    println!("{}", acc.unwrap());
+    println!("{}", acc);
     Ok(())
 }
