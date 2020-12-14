f6e3bc7dc3597a539fa633bd7cd47de5239e8c4e
Henrik Alsér <henrik.alser@me.com>
2020-12-14 11:15:00 +0100

    Cleanup

--- src/bin/14a.rs
+++ src/bin/14a.rs
@@ -1,26 +1,30 @@
 use std::io::{BufRead, BufReader, Error};
 use std::{collections::HashMap, fs::File};
 
-#[derive(Debug)]
 enum Command {
     SetMask(String),
     WriteMem(usize, i64),
 }
 
-fn command_from_string(s: String) -> Command {
-    match s.starts_with("mem") {
-        true => {
-            let cmd = s
-                .split(" = ")
-                .map(|x| {
-                    x.trim_matches(|c: char| !c.is_numeric())
-                        .parse::<i64>()
-                        .unwrap()
-                })
-                .collect::<Vec<_>>();
-            Command::WriteMem(*cmd.get(0).unwrap() as usize, *cmd.get(1).unwrap())
+impl Command {
+    fn from_string(s: String) -> Self {
+        match s.starts_with("mem") {
+            true => {
+                match s
+                    .split(" = ")
+                    .map(|x| {
+                        x.trim_matches(|c: char| !c.is_numeric())
+                            .parse::<i64>()
+                            .unwrap()
+                    })
+                    .collect::<Vec<_>>()[..]
+                {
+                    [addr, value] => Self::WriteMem(addr as usize, value),
+                    _ => panic!("Invalid data"),
+                }
+            }
+            _ => Self::SetMask(s.trim_start_matches("mask = ").to_owned()),
         }
-        _ => Command::SetMask(s.trim_start_matches("mask = ").to_owned()),
     }
 }
 
@@ -32,7 +36,7 @@ fn apply_mask(mask: &str, value: i64) -> i64 {
 fn main() -> Result<(), Error> {
     let commands = BufReader::new(File::open("./input/14.txt")?)
         .lines()
-        .map(|l| l.map(command_from_string))
+        .map(|l| l.map(Command::from_string))
         .collect::<Result<Vec<_>, _>>()?;
     let (_, mem) = commands.iter().fold(
         (String::with_capacity(36), HashMap::new()),
@@ -46,7 +50,7 @@ fn main() -> Result<(), Error> {
             (mask, mem)
         },
     );
-    let sum: i64 = mem.values().fold(0, |sum, v| sum + *v);
+    let sum = mem.values().fold(0, |sum, v| sum + *v);
     println!("{}", sum);
     Ok(())
 }
